e74e6602865e2efe9d607ef273d0dc521f51db38
Amin Tahmasbi <atahmasbi@hra.com>
2018-12-03 00:27:02 -0500

    fix: Add static dir to Flask

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,6 +4,6 @@ This application runs the back-end of the data project in my portfolio.
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
 
 from myapp import routes
