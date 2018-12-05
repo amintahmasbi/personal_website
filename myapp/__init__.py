29223770e9fda0ac84009a9717dce258bf1c5cb1
Amin Tahmasbi <atahmasbi@hra.com>
2018-12-05 00:44:35 -0500

    fix: Update routes to render static files

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,6 +4,8 @@ This application runs the back-end of the data project in my portfolio.
 
 from flask import Flask
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__)
+app.config['publishDir'] = 'myapp/templates/'
+app.config['root_html'] = 'index.html'
 
 from myapp import routes
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,33 +1,31 @@
 """route rules for flask
 """
-
-from flask import render_template
 from myapp import app
-from bs4 import BeautifulSoup
-import os
-import re
 
-@app.route('/')
-@app.route('/index')
-def index():
-    """redirect index url call to index.html"""
-    title = 'My website'
-    html = "index.html"
-    html_path = os.path.join('myapp','templates',html)
-    # Parse html
-    soup = None
-    with open(html_path, "r+") as html_file:
-        soup = BeautifulSoup(html_file, 'html5lib')
-        # print(soup.prettify())
-        for link in soup.find_all(href=re.compile("\.\/")):
-            link['href'] = link['href'].replace("./","templates/",1)
-        for src in soup.find_all(src=re.compile("\.\/")):
-            src['src'] = src['src'].replace("./","templates/",1)
-        for div in soup.find_all("div", class_="page-header-image", style=re.compile("\/img")):
-            if 'templates' not in div['style']:
-                div['style'] = div['style'].replace("/img","templates/img",1)
-    with open(html_path, "w") as html_file:
-        html_file.write(str(soup))
-    # Find the div with the class "full_name", show text
-    # print(soup.find("link", { "rel" : "stylesheet" }).text)
-    return render_template(html, title=title)
+from flask import send_from_directory, safe_join
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    dir = safe_join(app.config['publishDir'], path)
+    return send_from_directory(dir, app.config['root_html'] )
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    dir = safe_join(app.config['publishDir'], 'js')
+    return send_from_directory(dir, path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    dir = safe_join(app.config['publishDir'], 'css') 
+    return send_from_directory(dir, path)
+
+@app.route('/fonts/<path:path>')
+def send_fonts(path):
+    dir = safe_join(app.config['publishDir'], 'fonts') 
+    return send_from_directory(dir, path)
+
+@app.route('/img/<path:path>')
+def send_img(path):
+    dir = safe_join(app.config['publishDir'], 'img') 
+    return send_from_directory(dir, path)
