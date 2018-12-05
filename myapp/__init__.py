6de008fa342fab1020fef8166274b3d4d71fc792
Amin Tahmasbi <atahmasbi@hra.com>
2018-12-05 01:00:48 -0500

    fix: correct publish dir in package

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,7 +5,7 @@ This application runs the back-end of the data project in my portfolio.
 from flask import Flask
 
 app = Flask(__name__)
-app.config['publishDir'] = 'myapp/templates/'
+app.config['publishDir'] = 'templates/'
 app.config['root_html'] = 'index.html'
 
 from myapp import routes
