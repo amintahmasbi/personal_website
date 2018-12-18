0fbf40a54733c49879456a3235339339329d5493
Amin Tahmasbi <atahmasbi@hra.com>
2018-12-18 10:25:22 -0500

    style: fix all Linter warnings and infos

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,71 +1,73 @@
 """route rules for flask
 """
-import requests
 import os
 from functools import wraps
-from flask import request
+
+import requests
 
 from myapp import app
 
 from flask import send_from_directory, safe_join
+from flask import request
 from flask import jsonify
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    dir = safe_join(app.config['publishDir'], path)
-    return send_from_directory(dir, app.config['root_html'] )
+    """Handle all static html files from Hugo"""
+    file_dir = safe_join(app.config['publishDir'], path)
+    return send_from_directory(file_dir, app.config['root_html'])
 
 @app.route('/js/<path:path>')
 def send_js(path):
-    dir = safe_join(app.config['publishDir'], 'js')
-    return send_from_directory(dir, path)
+    """Redirect js files from Hugo"""
+    file_dir = safe_join(app.config['publishDir'], 'js')
+    return send_from_directory(file_dir, path)
 
 @app.route('/css/<path:path>')
 def send_css(path):
-    dir = safe_join(app.config['publishDir'], 'css')
-    return send_from_directory(dir, path)
+    """Redirect css files form Hugo"""
+    file_dir = safe_join(app.config['publishDir'], 'css')
+    return send_from_directory(file_dir, path)
 
 @app.route('/fonts/<path:path>')
 def send_fonts(path):
-    dir = safe_join(app.config['publishDir'], 'fonts')
-    return send_from_directory(dir, path)
+    """Redirect font files form Hugo"""
+    file_dir = safe_join(app.config['publishDir'], 'fonts')
+    return send_from_directory(file_dir, path)
 
 @app.route('/img/<path:path>')
 def send_img(path):
-    dir = safe_join(app.config['publishDir'], 'img')
-    return send_from_directory(dir, path)
+    """Redirect image files form Hugo"""
+    file_dir = safe_join(app.config['publishDir'], 'img')
+    return send_from_directory(file_dir, path)
 
 
-def check_recaptcha(f):
+def check_recaptcha(func):
     """
     Checks Google  reCAPTCHA.
 
     :param f: view function
     :return: Function
     """
-    @wraps(f)
+    @wraps(func)
     def decorated_function(*args, **kwargs):
+        """wrapper function"""
         request.recaptcha_is_valid = None
         private_key = os.environ['RECAPTCHA_PRIVATE_KEY']
         if request.method == 'POST':
-            data = {
-            'secret': private_key,
-            'response': request.form.get('g-recaptcha-response'),
-            'remoteip': request.access_route[0]
-            }
+            data = {'secret': private_key,
+                    'response': request.form.get('g-recaptcha-response'),
+                    'remoteip': request.access_route[0]}
 
-            r = requests.post("https://www.google.com/recaptcha/api/siteverify",
-            data=data)
+            res = requests.post("https://www.google.com/recaptcha/api/siteverify",
+                                data=data)
 
-            result = r.json()
+            result = res.json()
 
-            if result['success']:
-                request.recaptcha_is_valid = True
-            else:
-                request.recaptcha_is_valid = False
+            request.recaptcha_is_valid = bool(result['success'])
 
-        return f(*args, **kwargs)
+        return func(*args, **kwargs)
 
     return decorated_function
 
@@ -73,9 +75,14 @@ def check_recaptcha(f):
 @check_recaptcha
 def show_email():
     """Handle the email show request"""
+
+    response = None
     my_email = os.environ['MY_EMAIL']
+
     if request.recaptcha_is_valid:
-        return jsonify({'email': my_email})
+        response = jsonify({'email': my_email})
+
+    return response
 
 
 
